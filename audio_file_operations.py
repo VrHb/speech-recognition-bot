a6aa2712094cb5c4bc3c961297aad83ba5f13fcf
Victor Abdukarimov <viredmi@gmail.com>
2022-03-21 22:08:30 +0300

    move some func to module

---
+++ audio_file_operations.py
@@ -0,0 +1,33 @@
+import asyncio
+
+import speech_recognition as sr
+
+class ffmpeg():
+    PROGRAM = "ffmpeg"
+    FLAG1 = '-i'
+    FILE_IN = 'voice.ogg'
+    FILE_OUT = 'voice.wav'
+    FLAG2 = '-y'
+
+
+async def audio_convert() -> None:
+    """Run the convert process by ffmpeg converter with asyncio subprocess"""
+    process = await asyncio.create_subprocess_exec(
+            ffmpeg.PROGRAM, ffmpeg.FLAG1, 
+            ffmpeg.FILE_IN, ffmpeg.FILE_OUT, ffmpeg.FLAG2
+            )
+    await process.wait()
+
+
+async def audio_recognition(file: str = ffmpeg.FILE_OUT) -> str:
+    """Process recognize speech from audio file"""
+    r = sr.Recognizer()
+    user_audio_file = sr.AudioFile(file)
+    with user_audio_file as source:
+        user_audio = r.record(source)
+    try:
+        message_text = r.recognize_google(user_audio, language='ru-RU')
+        return str(message_text)
+    except sr.UnknownValueError:
+        return "Не понятно!"
+
--- server.py
+++ server.py
@@ -1,28 +1,20 @@
 import os
-import asyncio
-
-import speech_recognition as sr
-
 from aiogram import Bot, Dispatcher, executor, types
-
 from loguru import logger
 
+from audio_file_operations import audio_convert, audio_recognition 
 
 
-API_TOKEN = os.getenv("SPIKE_API_KEY")
 
 logger.add(
         "debug.log", format='{time} {level} {message}',
         level='DEBUG', serialize=True
         )
 
+API_TOKEN = os.getenv("SPIKE_API_KEY")
+
 bot = Bot(token=(str(API_TOKEN)))
 dp = Dispatcher(bot)
-
-@logger.catch
-async def audio_convert(
-        program: str, flag1: str, file_out: str, flag2: str) -> None:
-    await asyncio.create_subprocess_exec(program, flag1, file_out, flag2)
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -37,27 +29,19 @@ async def send_welcome(message: types.Message) -> None:
         parse_mode="MarkdownV2"
     )
 
+
 @dp.message_handler(content_types=['voice', 'audio'])
 async def get_audio_messages(message: types.Message) -> None:
     """get audio from user and download to server .ogg file"""
-    r = sr.Recognizer()
     file_id = message.voice.file_id
     file = await bot.get_file(file_id)
     file_path = file.file_path
+    logger.info(file_path)
     await bot.download_file(file_path, "voice.ogg")
-    # get audio file from telegram 
-    await audio_convert(
-            program='ffmpeg', 
-            flag1='-i', 
-            file_out='voice.wav',
-            flag2='-y')
-    # start new function
-    user_audio_file = sr.AudioFile("voice.wav")
-    with user_audio_file as source:
-        user_audio = r.record(source, duration=6)
-    text = r.recognize_google(user_audio, language='ru-RU')
-    print(text)
-    await message.answer(message.text)
-    
+    await audio_convert()
+    message_text = await audio_recognition()
+    await message.answer(message_text)
+     
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
